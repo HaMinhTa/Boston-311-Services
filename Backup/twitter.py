b713d4883e6c0877339d995fa99592338508cc8f
HaMinhTa <45018384+HaMinhTa@users.noreply.github.com>
2019-04-13 21:57:25 -0400

    Delete twitter.py

--- Backup/twitter.py
+++
@@ -1,7 +0,0 @@
-import requests
-import json
-
-response = requests.get("https://api.twitter.com/1.1/search/tweets.json?@BOS311&result_type=popular")
-
-data = json.loads(response.text)
-print(data)
