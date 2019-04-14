d9f9d22e010761bece30132f56f09871c7f77c9f
HaTa <ta.h@husky.neu.edu>
2019-04-14 12:32:51 -0400

    Clean up index.html indentation and refactor bubble chart functions

--- js/bubblechart.js
+++ js/bubblechart.js
@@ -2,15 +2,14 @@
 const LIGHT_BLUE = "#b3f0ff";
 const BLUE = "#00ccff";
 const ORANGE ="#ffbb33";
+const GRAY = "#D3D3D3";
 
-// var width = document.querySelector("#bubblechart").clientWidth + 500,
-// height = 500;
 var width = 800,
-height = 700;
+    height = 700;
 
 var svg = d3.select("#bubblechart").append("svg")
-  .attr("width",width)
-  .attr("height",height);
+  .attr("width", width)
+  .attr("height", height);
 
 var node_data = [
   {"reason": "Sanitation", "number": 27208, "year": 2012},
@@ -19,7 +18,9 @@ var node_data = [
   {"reason": "Street Cleaning", "number": 9967, "year": 2012},
   {"reason": "Highway Maintenance", "number": 23303, "year": 2012},
   {"reason": "Housing", "number": 7251, "year": 2012},
-
+  {"reason": "Signs & Signals", "number": 6340, "year": 2012},
+  {"reason": "Recycling", "number": 5202, "year": 2012},
+  {"reason": "Graffiti", "number": 3985, "year": 2012},
 
   {"reason": "Sanitation", "number": 15196, "year": 2018},
   {"reason": "Trees", "number": 4634, "year": 2018},
@@ -27,18 +28,12 @@ var node_data = [
   {"reason": "Street Cleaning", "number": 18311, "year": 2018},
   {"reason": "Highway Maintenance", "number": 15212, "year": 2018},
   {"reason": "Housing", "number": 3607, "year": 2018},
-
-  {"reason": "Signs & Signals", "number": 6340, "year": 2012},
   {"reason": "Signs & Signals", "number": 7865, "year": 2018},
-
-  {"reason": "Recycling", "number": 5202, "year": 2012},
   {"reason": "Recycling", "number": 4285, "year": 2018},
-
-  {"reason": "Graffiti", "number": 3985, "year": 2012},
   {"reason": "Graffiti", "number": 1779, "year": 2018}
 ];
 
-
+/* Defines a grid of (x,y) coordinates to position bubbles when categorized by type */
 const grid = [
   {x: width / 4,       y: height / 4      },
   {x: width / 2,       y: height / 4      },
@@ -63,9 +58,11 @@ const forceSplit = d3.forceX(d => {
   if (d["year"] === 2012) { return width / 4; }
   else                    { return (3 * width) / 4; }
 }).strength(0.5);
+
 const forceSplitByCategoryX = d3.forceX(d => {
   var offset = getRadius(d);
   if (d["year"] === 2012) { offset *= -1; }
+
   switch (d["reason"]) {
     case "Sanitation":
       return grid[0].x + offset;
@@ -87,6 +84,7 @@ const forceSplitByCategoryX = d3.forceX(d => {
       return grid[8].x + offset;
   }
 }).strength(0.4);
+
 const forceSplitByCategoryY = d3.forceY(d => {
   switch (d["reason"]) {
     case "Sanitation":
@@ -109,12 +107,14 @@ const forceSplitByCategoryY = d3.forceY(d => {
       return grid[8].y;
   }
 }).strength(0.4);
-const forceJoin = d3.forceX(d => width / 2).strength(0.07);
-const forceY = d3.forceY(height / 2).strength(0.07);
-const forceCenter = d3.forceCenter(width / 2, height / 2);
-const forceCollide = d3.forceCollide(d => getRadius(d) + 2).iterations(3);
-const forceCharge = d3.forceManyBody().strength(5);
 
+const forceJoin = d3.forceX(d => width / 2).strength(0.07);
+
+const forceY = d3.forceY(height / 2).strength(0.07);
+
+const forceCenter = d3.forceCenter(width / 2, height / 2);
+
+const forceCollide = d3.forceCollide(d => getRadius(d) + 2).iterations(3);
 
 var simulation = d3.forceSimulation()
                    .nodes(node_data)
@@ -122,7 +122,6 @@ var simulation = d3.forceSimulation()
                    .force('y', forceY)
                    .force('center', forceCenter)
                    .force('collide', forceCollide)
-                   .force('charge', forceCharge)
                    .on("tick", tickActions);
 
 var node = svg.append("g")
@@ -137,14 +136,14 @@ var node = svg.append("g")
                 else                    { return ORANGE; }
               })
               .on("mouseover", function(d,i) {
-                d3.select(this).attr("fill", "#D3D3D3")
+                d3.select(this).attr("fill", GRAY)
                 tooltip.transition().duration(100)
                 tooltip.html(`Reason: <b>${d.reason}</b> <br>Number of Calls: <b>${d.number}</b>`)
-                .style("left", d3.event.pageX - 80 + "px")
-                .style("top", d3.event.pageY - 80 + "px")
-                .style("opacity", 1)
-                .style("padding", "8px 10px")
-                .style("border-radius", "5px");
+                  .style("left", d3.event.pageX - 80 + "px")
+                  .style("top", d3.event.pageY - 80 + "px")
+                  .style("opacity", 1)
+                  .style("padding", "8px 10px")
+                  .style("border-radius", "5px");
               })
               .on("mouseout", function() {
                 d3.select(this).attr("fill",function(d) {
@@ -152,7 +151,7 @@ var node = svg.append("g")
                   else                    { return ORANGE; }
                 })
                 tooltip.html("")
-                .style("padding", "0");
+                  .style("padding", "0");
               });
 
 var bluerect = svg.append("rect")
@@ -161,7 +160,7 @@ var bluerect = svg.append("rect")
                   .attr("y", 40)
                   .attr("width", 25)
                   .attr("height", 25)
-                  .attr("fill", "#00ccff");
+                  .attr("fill", BLUE);
 
 var orangerect = svg.append("rect")
                   .attr("class", "legend")
@@ -169,50 +168,41 @@ var orangerect = svg.append("rect")
                   .attr("y", 40)
                   .attr("width", 25)
                   .attr("height", 25)
-                  .attr("fill", "#ffbb33");
+                  .attr("fill", ORANGE);
 
 var year12 = svg.append("text").text("2012")
                   .attr("x", 200)
                   .attr("y", 62)
-                  .attr("fill", "#ffffff")
+                  .attr("fill", "white")
                   .attr("class", "legend");
-
 
 var year18 = svg.append("text").text("2018")
                   .attr("x", 550)
                   .attr("y", 62)
-                  .attr("fill", "#ffffff")
+                  .attr("fill", "white")
                   .attr("class", "legend");
-
-
 
 function tickActions() {
     node.attr("transform", translateCircle);
-  }
+}
 
-// var tooltip = d3.select("body")
-//   .append("div")
-//   .attr("class", "tooltip")
-//   .style("position", "absolute")
-//   .style("background", "yellow");
+function noCategorization() {
+  simulation.force('x', forceJoin)
+            .force('y', forceY)
+            .alphaTarget(0.1)
+            .restart()
+}
 
-// d3.select("#btnYear").on('click', function() {
-  // simulation.force('x', forceSplit)
-  //           .force('y', forceY)
-  //           .alphaTarget(0.3)
-  //           .restart();
-// });
+function categorizeByYear() {
+  simulation.force('x', forceSplit)
+            .force('y', forceY)
+            .alphaTarget(0.3)
+            .restart()
+}
 
-// d3.select("#btnAll").on('click', function() {
-  // simulation.force('x', forceJoin)
-  //           .force('y', forceY)
-  //           .alphaTarget(0.1)
-  //           .restart();
-// });
-
-// d3.select("#btnCategory").on('click', function() {
-  // simulation.force('x', forceSplitByCategoryX)
-  //           .force('y', forceSplitByCategoryY)
-  //           .alphaTarget(0.1)
-  //           .restart();
-// });
+function categorizeByType() {
+  simulation.force('x', forceSplitByCategoryX)
+            .force('y', forceSplitByCategoryY)
+            .alphaTarget(0.1)
+            .restart()
+}
--- js/waypoints.js
+++ js/waypoints.js
@@ -19,8 +19,9 @@ var WAYPOINT = new Waypoint({
 
 */
 
+/* ----- BARCHART 2012 CALLS ----- */
 var WAYPOINT0 = new Waypoint({
-  element: document.querySelector("#trigger1"),
+  element: document.querySelector("#triggerBarChart2012"),
   handler: function(direction) {
     if(direction === "down") {
     d3.selectAll(".calls")
@@ -33,8 +34,9 @@ var WAYPOINT0 = new Waypoint({
   }
 });
 
+/* ----- BARCHART 2018 CALLS ----- */
 var WAYPOINT1 = new Waypoint({
-  element: document.querySelector("#trigger2"),
+  element: document.querySelector("#triggerBarChart2018"),
   offset: 500,
   handler: function(direction) {
     if(direction === "down") {
@@ -48,9 +50,9 @@ var WAYPOINT1 = new Waypoint({
   }
 });
 
-
+/* ----- LINECHART ----- */
 var WAYPOINT2 = new Waypoint({
-  element: document.querySelector("#trigger3"),
+  element: document.querySelector("#triggerLineChart"),
   offset: 100,
   handler: function(direction) {
     if(direction === "down") {
@@ -85,73 +87,173 @@ var WAYPOINT2 = new Waypoint({
 }
 });
 
+/* ----- PIE CHART ----- */
+// var WAYPOINT3 = new Waypoint({
+//   element: document.querySelector("#trigger4"),
+//   handler: function(direction) {
+//     if(direction === "down") {
+//       var pie = new d3pie("#piechart", {
+//       	"header": {
+//       		"title": {
+//       			"text": "Boston 311 Medium of Calls",
+//       			"fontSize": 26,
+//       			"font": "open sans"
+//       		},
+//       		"subtitle": {
+//       			"text": "How Bostonians request 311 services",
+//       			"color": "#999999",
+//       			"fontSize": 15,
+//       			"font": "open sans"
+//       		},
+//       		"titleSubtitlePadding": 11
+//       	},
+//       	"footer": {
+//       		"color": "#999999",
+//       		"fontSize": 10,
+//       		"font": "open sans",
+//       		"location": "bottom-left"
+//       	},
+//       	"size": {
+//       		"canvasHeight": 700,
+//       		"canvasWidth": 800,
+//       		"pieOuterRadius": "94%"
+//       	},
+//       	"data": {
+//       		"content": [
+//       			{
+//       				"label": "Citizen Connect App",
+//       				"value": 330570,
+//       				"color": "#01c9ff"
+//       			},
+//       			{
+//       				"label": "City Worker App",
+//       				"value": 110319,
+//       				"color": "#097bee"
+//       			},
+//       			{
+//       				"label": "Constituent Calls",
+//       				"value": 459601,
+//       				"color": "#ffd009"
+//       			},
+//       			{
+//       				"label": "Employee Generated",
+//       				"value": 72299,
+//       				"color": "#ff00dd"
+//       			},
+//       			{
+//       				"label": "Maximo Integration",
+//       				"value": 4104,
+//       				"color": "#990000"
+//       			},
+//       			{
+//       				"label": "Self Service",
+//       				"value": 70124,
+//       				"color": "#07fdac"
+//       			},
+//       			{
+//       				"label": "Twitter",
+//       				"value": 1558,
+//       				"color": "#65cc32"
+//       			}
+//       		]
+//       	},
+//       	"labels": {
+//       		"outer": {
+//       			"pieDistance": 32
+//       		},
+//       		"inner": {
+//       			"hideWhenLessThanPercentage": 1
+//       		},
+//       		"mainLabel": {
+//       			"fontSize": 14
+//       		},
+//       		"percentage": {
+//       			"color": "#ffffff",
+//       			"fontSize": 17,
+//       			"decimalPlaces": 1
+//       		},
+//       		"value": {
+//       			"color": "#ffffff",
+//       			"fontSize": 13
+//       		},
+//       		"lines": {
+//       			"enabled": true
+//       		},
+//       		"truncation": {
+//       			"enabled": true
+//       		}
+//       	},
+//       	"tooltips": {
+//       		"enabled": true,
+//       		"type": "placeholder",
+//       		"string": "{label}: {percentage}%, {value} calls",
+//       		"styles": {
+//       			"backgroundColor": "#13e4f8",
+//       			"backgroundOpacity": 0.71,
+//       			"color": "#030303",
+//       			"fontSize": 14
+//       		}
+//       	},
+//       	"effects": {
+//       		"pullOutSegmentOnClick": {
+//       			"effect": "linear",
+//       			"speed": 400,
+//       			"size": 8
+//       		}
+//       	},
+//       	"misc": {
+//       		"gradient": {
+//       			"enabled": true,
+//       			"percentage": 100
+//       		}
+//       	}
+//       });    }
+//       else if(direction === "up") {}
+//   }
+// });
 
-var WAYPOINT3 = new Waypoint({
-  element: document.querySelector("#trigger5a"),
-  offset: 100,
-  handler: function(direction) {
-    if(direction === "down") {
-      d3.selectAll(".bubble")
-      simulation.force('x', forceJoin)
-                .force('y', forceY)
-                .alphaTarget(0.1)
-                .restart()
-
-    } else if(direction === "up") {
-      d3.selectAll(".bubble")
-      simulation.force('x', forceJoin)
-                .force('y', forceY)
-                .alphaTarget(0.1)
-                .restart()
-    }
-  }
-});
-
+/* ----- BUBBLE CHART ----- */
 var WAYPOINT4 = new Waypoint({
-  element: document.querySelector("#trigger5b"),
+  element: document.querySelector("#triggerBubbleChart"),
   offset: 100,
   handler: function(direction) {
     if(direction === "down") {
-      d3.selectAll(".bubble")
-      simulation.force('x', forceSplit)
-                .force('y', forceY)
-                .alphaTarget(0.3)
-                .restart()
-
+      noCategorization();
     } else if(direction === "up") {
-      d3.selectAll(".bubble")
-      simulation.force('x', forceSplit)
-                .force('y', forceY)
-                .alphaTarget(0.3)
-                .restart()
+      noCategorization();
     }
   }
 });
 
-
-var WAYPOINT5 = new Waypoint({
-  element: document.querySelector("#trigger5c"),
+/* ----- BUBBLE CHART CATEGORIZE BY YEAR ----- */
+var WAYPOINT4b = new Waypoint({
+  element: document.querySelector("#triggerBubbleChartYear"),
   offset: 100,
   handler: function(direction) {
     if(direction === "down") {
-      d3.selectAll(".bubble")
-      simulation.force('x', forceSplitByCategoryX)
-                .force('y', forceSplitByCategoryY)
-                .alphaTarget(0.1)
-                .restart()
-
+      categorizeByYear();
     } else if(direction === "up") {
-      d3.selectAll(".bubble")
-      simulation.force('x', forceSplitByCategoryX)
-                .force('y', forceSplitByCategoryY)
-                .alphaTarget(0.1)
-                .restart()
+      categorizeByYear();
     }
   }
 });
 
+/* ----- BUBBLE CHART CATEGORIZE BY TYPE */
+var WAYPOINT4c = new Waypoint({
+  element: document.querySelector("#triggerBubbleChartType"),
+  offset: 100,
+  handler: function(direction) {
+    if(direction === "down") {
+      categorizeByType();
+    } else if(direction === "up") {
+      categorizeByType();
+    }
+  }
+});
+
+/* ----- BALLOON CHART ----- */
 var WAYPOINT6 = new Waypoint({
-  element: document.querySelector("#trigger4"),
+  element: document.querySelector("#triggerBalloonChart"),
   offset: 100,
   handler: function(direction) {
     if(direction === "down") {
