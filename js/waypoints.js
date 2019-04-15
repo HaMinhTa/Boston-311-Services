63b4f98a5491d4dfea74080b00f2fad43ac4fe1c
HaTa <ta.h@husky.neu.edu>
2019-04-15 10:54:02 -0400

    Add piechart and tweets

--- js/waypoints.js
+++ js/waypoints.js
@@ -93,122 +93,126 @@ var WAYPOINT3 = new Waypoint({
   handler: function(direction) {
     if(direction === "down") {
       var pie = new d3pie("#piechart", {
-      	"header": {
-      		"title": {
-      			"text": "Boston 311 Medium of Calls",
-      			"fontSize": 26,
-      			"font": "open sans"
-      		},
-      		"subtitle": {
-      			"text": "How Bostonians request 311 services",
-      			"color": "#999999",
-      			"fontSize": 15,
-      			"font": "open sans"
-      		},
-      		"titleSubtitlePadding": 11
-      	},
-      	"footer": {
-      		"color": "#999999",
-      		"fontSize": 10,
-      		"font": "open sans",
-      		"location": "bottom-left"
-      	},
-      	"size": {
-      		"canvasHeight": 700,
-      		"canvasWidth": 800,
-      		"pieOuterRadius": "94%"
-      	},
-      	"data": {
-      		"content": [
-      			{
-      				"label": "Citizen Connect App",
-      				"value": 330570,
-      				"color": "#01c9ff"
-      			},
-      			{
-      				"label": "City Worker App",
-      				"value": 110319,
-      				"color": "#097bee"
-      			},
-      			{
-      				"label": "Constituent Calls",
-      				"value": 459601,
-      				"color": "#ffd009"
-      			},
-      			{
-      				"label": "Employee Generated",
-      				"value": 72299,
-      				"color": "#ff00dd"
-      			},
-      			{
-      				"label": "Maximo Integration",
-      				"value": 4104,
-      				"color": "#990000"
-      			},
-      			{
-      				"label": "Self Service",
-      				"value": 70124,
-      				"color": "#07fdac"
-      			},
-      			{
-      				"label": "Twitter",
-      				"value": 1558,
-      				"color": "#65cc32"
-      			}
-      		]
-      	},
-      	"labels": {
-      		"outer": {
-      			"pieDistance": 32
-      		},
-      		"inner": {
-      			"hideWhenLessThanPercentage": 1
-      		},
-      		"mainLabel": {
-      			"fontSize": 14
-      		},
-      		"percentage": {
-      			"color": "#ffffff",
-      			"fontSize": 17,
-      			"decimalPlaces": 1
-      		},
-      		"value": {
-      			"color": "#ffffff",
-      			"fontSize": 13
-      		},
-      		"lines": {
-      			"enabled": true
-      		},
-      		"truncation": {
-      			"enabled": true
-      		}
-      	},
-      	"tooltips": {
-      		"enabled": true,
-      		"type": "placeholder",
-      		"string": "{label}: {percentage}%, {value} calls",
-      		"styles": {
-      			"backgroundColor": "#13e4f8",
-      			"backgroundOpacity": 0.71,
-      			"color": "#030303",
-      			"fontSize": 14
-      		}
-      	},
-      	"effects": {
-      		"pullOutSegmentOnClick": {
-      			"effect": "linear",
-      			"speed": 400,
-      			"size": 8
-      		}
-      	},
-      	"misc": {
-      		"gradient": {
-      			"enabled": true,
-      			"percentage": 100
-      		}
-      	}
-      });    }
-      else if(direction === "up") {}
+        	"header": {
+        		"title": {
+        			"text": "Boston 311 Medium of Calls",
+        			"color": "#ffffff",
+        			"fontSize": 26,
+        			"font": "open sans"
+        		},
+        		"subtitle": {
+        			"text": "How Bostonians request 311 services",
+        			"color": "#999999",
+        			"fontSize": 16,
+        			"font": "open sans"
+        		},
+        		"titleSubtitlePadding": 12
+        	},
+        	"footer": {
+        		"color": "#999999",
+        		"fontSize": 10,
+        		"font": "open sans",
+        		"location": "bottom-left"
+        	},
+        	"size": {
+        		"canvasHeight": 720,
+        		"canvasWidth": 800,
+        		"pieOuterRadius": "93%"
+        	},
+        	"data": {
+        		"content": [
+        			{
+        				"label": "Citizen Connect App",
+        				"value": 330570,
+        				"color": "#00c9ff"
+        			},
+        			{
+        				"label": "City Worker App",
+        				"value": 110319,
+        				"color": "#128bd7"
+        			},
+        			{
+        				"label": "Constituent Calls",
+        				"value": 459601,
+        				"color": "#fbe901"
+        			},
+        			{
+        				"label": "Employee Generated",
+        				"value": 72299,
+        				"color": "#eb14dc"
+        			},
+        			{
+        				"label": "Maximo Integration",
+        				"value": 4104,
+        				"color": "#ff0035"
+        			},
+        			{
+        				"label": "Self Service",
+        				"value": 70124,
+        				"color": "#00f897"
+        			},
+        			{
+        				"label": "Twitter",
+        				"value": 1558,
+        				"color": "#2426e6"
+        			}
+        		]
+        	},
+        	"labels": {
+        		"outer": {
+        			"pieDistance": 26
+        		},
+        		"inner": {
+        			"hideWhenLessThanPercentage": 1
+        		},
+        		"mainLabel": {
+        			"color": "#f0ebeb",
+        			"fontSize": 15
+        		},
+        		"percentage": {
+        			"color": "#ffffff",
+        			"fontSize": 18,
+        			"decimalPlaces": 1
+        		},
+        		"value": {
+        			"color": "#adadad",
+        			"fontSize": 15
+        		},
+        		"lines": {
+        			"enabled": true
+        		},
+        		"truncation": {
+        			"enabled": true
+        		}
+        	},
+        	"tooltips": {
+        		"enabled": true,
+        		"type": "placeholder",
+        		"string": "{label}: {value} calls, {percentage}%",
+        		"styles": {
+        			"backgroundColor": "#121313",
+        			"backgroundOpacity": 0.48,
+        			"color": "#eeecec",
+        			"borderRadius": 3,
+        			"fontSize": 15,
+        			"padding": 5
+        		}
+        	},
+        	"effects": {
+        		"pullOutSegmentOnClick": {
+        			"effect": "linear",
+        			"speed": 400,
+        			"size": 12
+        		}
+        	},
+        	"misc": {
+        		"gradient": {
+        			"enabled": true,
+        			"percentage": 100
+        		}
+        	}
+        });
+    } else if(direction === "up") {}
   }
 });
 
